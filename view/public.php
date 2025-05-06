c73e4da5c63fa6d1b26859e2db15af9a33889322
LNI <a@b.com>
2025-05-06 11:31:19 +0200

    Update vue page d'accueil.

--- view/public.php
+++ view/public.php
@@ -3,10 +3,10 @@
     <h2>Derniers Articles</h2>
         <?php foreach ($args['listArticle'] as $article) { ?>
             <article class="article-item">
-                <h3><a href="/article/<?= $article['id'] ?>"><?= htmlspecialchars($article['title']) ?></a></h3>
+                <h3><a href="/ctrl/article-show.php?id=<?= $article['id'] ?>"><?= htmlspecialchars($article['title']) ?></a></h3>
                 <p class="publish-date">Publié le : <?= date('d/m/Y à H:i', strtotime($article['published_at'])) ?></p>
                 <p><?= substr(htmlspecialchars($article['content']), 0, 200) ?>...</p>
-                <p class="read-more"><a href="/article/<?= $article['id'] ?>">Lire la suite</a></p>
+                <p class="read-more"><a href="/ctrl/article-show.php?id=<?= $article['id'] ?>">Lire la suite</a></p>
             </article>
         <?php } ?>
     </section>
